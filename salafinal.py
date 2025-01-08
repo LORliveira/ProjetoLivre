d90c4d2cb66b46d4bc35a9ba8a98e7ce8f8f99bb
Loliveirinha <101184699+LORliveira@users.noreply.github.com>
2025-01-08 20:56:35 -0300

    Update salafinal.py

--- salafinal.py
+++ salafinal.py
@@ -3,6 +3,6 @@ from subsala import Subsala
 class SalaBossFinal(Subsala):
     def __init__(self, nome, boss, enigma, debuffar, dica_infusao):
         super().__init__(nome, boss, enigma, debuffar, dica_infusao)
-
+        self.salas = []
     def desbloqueado(self):
-        return all(sala.matou_boss for sala in self.salas)
+        return all(s.matou_boss for s in self.salas if s is not self)
